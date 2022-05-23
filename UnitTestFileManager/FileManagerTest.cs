748045332a09e9dc1ff5a04ca142b69acc3a50d4
Keri <79213053+KeriGabriel@users.noreply.github.com>
2022-05-23 11:41:10 -0700

    Add project files.

---
+++ UnitTestFileManager/FileManagerTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestFileManager
+{
+    [TestClass]
+    public class FileManagerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
